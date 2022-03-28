f3d1f93cec88d7163768f48d29512beb28f47147
tangxuliang <1297441127@qq.com>
2022-03-28 11:43:38 +0800

    test: hash测试

--- tests/RedisTest.php
+++ tests/RedisTest.php
@@ -1,13 +1,26 @@
 <?php
 
 declare(strict_types=1);
-it('remember', function () {
+it('rememberZset', function () {
     $response = redis()->type('zset')
         ->score(101)
         ->expire(3600)
-        ->remember('test_name', function () {
-            return 'test101';
+        ->remember('test_zset', function () {
+            return json_encode([
+                'name' => 'test101'
+            ]);
         });
 
-    expect($response)->toEqual('test101');
+    expect($response)->toEqual('{"name":"test101"}');
+});
+
+it('rememberHash', function () {
+    $response = redis()->type('hash')
+        ->hashKey('business_uuid')
+        ->expire(3600)
+        ->remember('test_hash', function () {
+            return 'jqw4iej6uj48dw8';
+        });
+
+    expect($response)->toEqual('jqw4iej6uj48dw8');
 });
